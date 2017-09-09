0e5d940c689fe11202d86da48d320e5132af60f4
Lauren <Laurenelee88@gmail.com>
2017-09-09 09:56:11 -0700

    finish methods for leap, robot, and hamming

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,20 @@
+class Robot
+  attr_reader :name
 
+  def initialize
+
+  @name = (("A".."Z").to_a.sample(2) + ("0".."9").to_a.sample(3)).join
+  end
+
+  def reset
+# @name = (/[A-Z]/.sample(2) + /[0-9]/.sample(3)).join
+  @name = (("A".."Z").to_a.sample(2) + ("0".."9").to_a.sample(3)).join
+  end
+end
+
+robot = Robot.new
+name = robot.name
+puts name
+robot.reset
+name2 = robot.name
+puts name2
